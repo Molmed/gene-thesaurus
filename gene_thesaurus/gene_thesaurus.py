9963f8e2f035ab96c830b2c48397b852c6590fc6
Mariya Lysenkova <mariya.lysenkova@medsci.uu.se>
2023-06-07 16:37:41 +0200

    Download latest gene file from hgnc based on current date

--- gene_thesaurus/gene_thesaurus.py
+++ gene_thesaurus/gene_thesaurus.py
@@ -1,17 +1,18 @@
 from pathlib import Path
+from datetime import datetime
 import os.path
 import requests
 import pandas as pd
 import json
 
 HGNC_BASE_URL = 'https://ftp.ebi.ac.uk/pub/databases/genenames/hgnc/archive/monthly/json/'
-HGNC_BASE_FILENAME = 'hgnc_complete_set_{date}.json'
-DATA_PATH = str(Path(__file__).parent.parent.absolute()) + '/data/'
-FALLBACK_DB_DATE = '2023-05-01'
+HGNC_BASE_FILENAME = 'hgnc_complete_set_{month}-01.json'
 
-def download_json(date):
-	filename = HGNC_BASE_FILENAME.format(date=date)
-	path = DATA_PATH + filename
+def download_json(data_dir):
+	month = datetime.today().strftime('%Y-%m')
+	filename = HGNC_BASE_FILENAME.format(month=month)
+
+	path = data_dir + "/" + filename
 
 	if os.path.isfile(path):
 		return path
@@ -23,27 +24,23 @@ def download_json(date):
 
 	return path
 
-def get_json(date):
-	json_path = download_json('2023-04-01', encoding='utf8')
+def get_json(date, data_dir):
+	json_path = download_json(data_dir)
 	
-	with open(json_path,'r') as f:
+	with open(json_path,'r', encoding='utf8') as f:
 		data = json.loads(f.read())
 
 	return data['response']['docs']
 
-def get_df(date):
-	json = get_json(date)
+def get_df(date, data_dir):
+	json = get_json(date, data_dir)
 	return pd.json_normalize(json)
 
 
-def lookup_by_prev_symbol(gene_name):
-	df = get_df('2023-04-01')
-	#print(df.columns)
-	#print(df['gtrnadb'])
+def lookup_by_prev_symbol(gene_name, data_dir):
+	df = get_df('2023-04-01', data_dir)
 	filtered_df = df[df['prev_symbol'].apply(lambda x: isinstance(x, list) and gene_name in x)]
 	return filtered_df[['gencc', 'hgnc_id', 'refseq_accession', 'symbol', 'ccds_id', 'omim_id', 'rgd_id', 'merops']]
 
-def lookup(gene_name):
-	return lookup_by_prev_symbol(gene_name)
-
-print(lookup('ZSCAN5CP'))
+def lookup(gene_name, data_dir='/tmp'):
+	return lookup_by_prev_symbol(gene_name, data_dir)
--- tests/test_gene_thesaurus.py
+++ tests/test_gene_thesaurus.py
@@ -1,6 +1,8 @@
 from gene_thesaurus import gene_thesaurus
+import tempfile
 
 def test_lookup():
     test_genes = ['TNFSF2', 'ERBB1', 'VPF', 'ZSCAN5CP']
-    symbols = gene_thesaurus.lookup(test_genes)
+    data_dir = "/tmp" #tempfile.TemporaryDirectory().name
+    symbols = gene_thesaurus.lookup(test_genes, data_dir=data_dir)
     assert symbols == ['TNF', 'EGFR', 'VEGFA', 'ZSCAN5C']
