190a09564f6a4a95c5823d780e336ed18a283e7f
Mariya Lysenkova <mariya.lysenkova@medsci.uu.se>
2023-06-07 17:07:10 +0200

    Add github actions temp dir support to test

--- tests/test_gene_thesaurus.py
+++ tests/test_gene_thesaurus.py
@@ -1,8 +1,12 @@
 from gene_thesaurus import gene_thesaurus
 import tempfile
+import os
 
 def test_lookup():
     test_genes = ['TNFSF2', 'ERBB1', 'VPF', 'ZSCAN5CP']
-    data_dir = tempfile.TemporaryDirectory().name
-    symbols = gene_thesaurus.lookup(test_genes, data_dir=data_dir)
+    data_dir_name = os.getenv('RUNNER_TEMP', None)
+    if not data_dir_name:
+        data_dir = tempfile.TemporaryDirectory()
+        data_dir_name = data_dir.name
+    symbols = gene_thesaurus.lookup(test_genes, data_dir=data_dir_name)
     assert symbols == ['TNF', 'EGFR', 'VEGFA', 'ZSCAN5C']
