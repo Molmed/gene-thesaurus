57ea9f97ed40deefc9b33a1ccef5fc1073d96116
Mariya Lysenkova <mariya.lysenkova@medsci.uu.se>
2023-10-27 16:42:45 +0200

    Better caching for gene thesaurus

--- src/gene_thesaurus.py
+++ src/gene_thesaurus.py
@@ -6,11 +6,13 @@ import json
 
 HGNC_BASE_URL = 'https://ftp.ebi.ac.uk/pub/databases/genenames/hgnc/archive/monthly/json/'
 HGNC_BASE_FILENAME = 'hgnc_complete_set_{month}-01.json'
+GENE_THESAURUS_BASE_FILENAME='gene_thesaurus_{month}-01.json'
 
-def get_json_path(data_dir):
-    month = datetime.today().strftime('%Y-%m')
-    filename = HGNC_BASE_FILENAME.format(month=month)
+def get_month():
+    return datetime.today().strftime('%Y-%m')
 
+def get_hgnc_json_path(data_dir):
+    filename = HGNC_BASE_FILENAME.format(month=get_month())
     path = data_dir + "/" + filename
 
     if os.path.isfile(path):
@@ -23,28 +25,58 @@ def get_json_path(data_dir):
 
     return path
 
-def get_json_data(data_dir):
-    json_path = get_json_path(data_dir)
+def get_hgnc_json_data(data_dir):
+    json_path = get_hgnc_json_path(data_dir)
     
     with open(json_path,'r', encoding='utf8') as f:
         data = json.loads(f.read())
 
     return data['response']['docs']
 
+def get_gene_thesaurus_dict(data_dir):
+    filename = GENE_THESAURUS_BASE_FILENAME.format(month=get_month())
+    json_path = data_dir + "/" + filename
+
+    # If dict already exists, return it
+    if os.path.isfile(json_path):
+        with open(json_path,'r', encoding='utf8') as f:
+            data = json.loads(f.read())
+            return data
+        
+    # Otherwise, construct it from the hgnc data and then return it
+    hgnc_json = get_hgnc_json_data(data_dir)
+
+    # Convert to simple dict, where each 'symbol', 'prev_symbol' and 'alias_symbol' all map to 'symbol'
+    gene_dict = {}
+    for item in hgnc_json:
+        # The current gene name
+        symbol = item.get("symbol")
+        gene_dict[symbol] = symbol
+
+        # Optionally available older synonyms
+        prev_symbols = item.get("prev_symbol", [])
+        for sym in prev_symbols:
+            gene_dict[sym] = symbol
+        alias_symbols = item.get("alias_symbol", [])
+        for sym in alias_symbols:
+            gene_dict[sym] = symbol
+
+    # Save the gene dict
+    with open(json_path, 'w') as file:
+        json.dump(gene_dict, file)
+
+    return gene_dict
 
 def translate_genes(gene_list, data_dir='/tmp'):
-    json = get_json_data(data_dir)
+    gene_thesaurus_dict = get_gene_thesaurus_dict(data_dir)
 
     results = []
     for gene in gene_list:
-        found = None
-        for item in json:
-            # Currently looks in two different fields: 'prev_symbol' and 'alias_symbol'
-            prev_symbol = item.get("prev_symbol", "")
-            alias_symbol = item.get("alias_symbol", "")
-
-            if gene.upper() in prev_symbol or gene.upper() in alias_symbol:
-                found = item.get("symbol")
-        results.append(found)
+        try:
+            symbol = gene_thesaurus_dict[gene]
+        except:
+            print("Could not find {}".format(gene))
+            symbol = None
+        results.append(symbol)
 
     return results
