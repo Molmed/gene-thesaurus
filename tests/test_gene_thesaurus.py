ea2087cb0fd5cbb6716571d03b80f830e8914042
Mariya Lysenkova <mariya.lysenkova@medsci.uu.se>
2023-11-01 10:33:51 +0100

    Remove trailing whitespace

--- tests/test_gene_thesaurus.py
+++ tests/test_gene_thesaurus.py
@@ -18,21 +18,21 @@ def test_get_month_first_day():
 
 def test_translate_genes():
     test_genes = ['TNFSF2', 'ERBB1', 'VPF', 'ZSCAN5CP']
-    symbols = gene_thesaurus.translate_genes(test_genes, 
+    symbols = gene_thesaurus.translate_genes(test_genes,
                                              data_dir=data_dir_name)
     assert symbols == ['TNF', 'EGFR', 'VEGFA', 'ZSCAN5C']
 
 
 def test_translate_invalid_genes():
     test_genes = ['TNFSF2', 'ERBB1', 'NOTAREALGENE', 'ZSCAN5CP']
-    symbols = gene_thesaurus.translate_genes(test_genes, 
+    symbols = gene_thesaurus.translate_genes(test_genes,
                                              data_dir=data_dir_name)
     assert symbols == ['TNF', 'EGFR', 'NOTAREALGENE', 'ZSCAN5C']
 
 
 def test_translate_invalid_genes_nullify_missing():
     test_genes = ['TNFSF2', 'ERBB1', 'NOTAREALGENE', 'ZSCAN5CP']
-    symbols = gene_thesaurus.translate_genes(test_genes, 
-                                             data_dir=data_dir_name, 
+    symbols = gene_thesaurus.translate_genes(test_genes,
+                                             data_dir=data_dir_name,
                                              nullify_missing=True)
     assert symbols == ['TNF', 'EGFR', None, 'ZSCAN5C']
